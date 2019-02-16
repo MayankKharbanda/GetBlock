0382aa9a36b9a3e2b74421886ce98242074c0ea3
Mayank Kharbanda <kharbanda.mayank@outlook.com>
2019-02-16 19:08:55 +0530

    before signals from child to main

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import get_block
 import threading
+import subprocess
+
+#TODO signal from process to main
+
 
 def process_creater():
+    
     for i in range(1,20):
-        #TODO call processes
-        print(i)
+        subprocess.Popen("process"+i+".py", shell=True)
 
 
 threading.Thread(target = process_creater).start()
