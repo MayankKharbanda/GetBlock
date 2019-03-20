1807437651265f2cceac5eda3d6fef3cf7f4440e
Mayank Kharbanda <kharbanda.mayank@outlook.com>
2019-03-20 12:50:16 +0530

    before comments and last checking

--- get_block.py
+++ get_block.py
@@ -1,23 +1,23 @@
-import buffer_cache
+#import buffer_cache
 
 
 
-def get_block(self, block_num):
+def get_block(buf_cache, block_num):
     
     
     
-    buf_cache = buffer_cache.Buffer_Cache()
+    #buf_cache = buffer_cache.Buffer_Cache()
     
     
     
     if buf_cache.in_hash_queue(block_num):
-        
+        print("in if getblock")
         block = buf_cache.assign_block(block_num)
         if block.get_status == "busy":
             print("a")#continue
         
         block.set_status("busy")
-        
+        print(block.get_status)
         buf_cache.free_list.remove(block)
         
         return block
@@ -25,7 +25,7 @@ def get_block(self, block_num):
     
     
     else:
-        
+        print("in else getblock")
         if buf_cache.free_list.is_Empty():
             print("a")#continue
         
@@ -33,8 +33,8 @@ def get_block(self, block_num):
         
         if(free_block.get_status == "delayed write"):
             print("write to disk")
-        
-        buf_cache.hash_queue_headers[(free_block.get_block_num())%64].remove(free_block)
+        if(free_block.get_block_num() != None):
+            buf_cache.hash_queue_headers[(free_block.get_block_num())%64].remove(free_block)
         free_block.set_block_num(block_num)
         buf_cache.hash_queue_headers[(free_block.get_block_num())%64]
         
--- kernel.py
+++ kernel.py
@@ -1,11 +1,17 @@
+import buffer_cache
 import get_block
-import threading
+#import threading
 import subprocess
+import time
+
 
 #TODO signal from process to main
 
-def read(self, blocknum, timestamp):
-    get_block.get_block(blocknum)
+buf_cache = buffer_cache.Buffer_Cache()
+
+def read(blocknum, timestamp):
+    print("in read")
+    get_block.get_block(buf_cache, blocknum)
     
 
 def write():
@@ -19,5 +25,17 @@ def process_creater():
     for i in range(1,20):
         subprocess.Popen("process"+i+".py", shell=True)
 
+#threading.Thread(target = process_creater).start()
 
-threading.Thread(target = process_creater).start()
+
+def main():
+    print("in main1")
+    read(345,200)
+    print("in main")
+    time.sleep(0.02)
+    #kernel.write(145,100)
+    #time.sleep(0.01)
+    #kernel.read(1235,200)
+    #time.sleep(0.02)
+if __name__=="__main__":
+    main()
