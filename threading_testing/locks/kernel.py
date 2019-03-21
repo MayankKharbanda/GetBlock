f47368523e8f82a6107a89687a4d314d1e825444
Mayank Kharbanda <kharbanda.mayank@outlook.com>
2019-03-21 20:26:54 +0530

    Process access from different files as threads

--- threading_testing/locks/kernel.py
+++ threading_testing/locks/kernel.py
@@ -1,6 +1,6 @@
 from shared_resource import SharedResource
 import threading
-import random
+#import random
 from time import sleep
 import logging
 
@@ -10,13 +10,14 @@ formatter = "%(asctime)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=formatter)
 logger = logging.getLogger(__name__)
 
-def logit(pid, res, status):
+def logit(pid, res, mode, status):
     res = f'resource {res}' if res is not None else " "*10
-    logger.info(f'Process {pid} - {res} - Status:{status}')
+    mode = f'Mode:{mode}  ' if mode is not None else " "*10
+    logger.info(f'Process {pid} - {res} - {mode} - Status:{status}')
 
 ######Shared Resources######
 
-SHARED_RESOURCES = 2
+SHARED_RESOURCES = 3
 
 shared_resource_list = []
 for i in range(SHARED_RESOURCES):
@@ -29,22 +30,48 @@ for i in range(SHARED_RESOURCES):
 ######Threads######
 
 def process(pid):
-    logit(pid, None, "started")
+    
+    try:
+        
+        proc = open("process"+str(pid)+".txt","r")
+        
+        #filehandling
+    except IOError:
+        
+        print("Error: can\'t find process "+ pid +" or read data")
+    
+    
+    else:
+        logit(pid, None, None, "started")
+        
+        proc_commands = proc.readlines()
+        for i in proc_commands:
+            
+            proc_command = i.split()
+            
+            '''
+            here proc_command contains data of the form
+            proc_command[0] contains mode of access -read/write
+            proc_command[1] contains block number to access
+            proc_command[2] contains time for access/sleep            
+            '''
+            
+            #res = random.randint(0, SHARED_RESOURCES-1)
+            
+            logit(pid, proc_command[1], proc_command[0], "trying to lock")
 
-    res = random.randint(0, SHARED_RESOURCES-1)
-    logit(pid, res, "trying to lock")
-
-    '''
-    with construct automatically calls
-    lock.acquire() when entering and
-    lock.release() when exiting
-    '''
-    with shared_resource_list[res].lock:
-        logit(pid, res, "locked")
-        sleep(1)
-        logit(pid, res, "releasing")
-
-    logit(pid, None, "exiting")
+            '''
+            with construct automatically calls
+            lock.acquire() when entering and
+            lock.release() when exiting
+            '''
+            
+            with shared_resource_list[int(proc_command[1])-1].lock:
+                logit(pid, proc_command[1], proc_command[0], "locked")
+                sleep(float(proc_command[2]))
+                logit(pid, proc_command[1], proc_command[0], "releasing")
+                
+        logit(pid, None, None, "exiting")
 
 def kernel():
     logger.info('Kernel started')
